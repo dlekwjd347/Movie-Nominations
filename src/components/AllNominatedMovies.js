2475545ea1cef6fa2bb6cfc4df52dc0302d19468
Elizabeth Lee <dlekwjd347@gmail.com>
2021-01-22 23:01:56 -0500

    formatted and styled nominated movies section

--- src/components/AllNominatedMovies.js
+++ src/components/AllNominatedMovies.js
@@ -4,39 +4,36 @@ import { connect } from 'react-redux';
 
 function AllNominatedMovies(props) {
   console.log(props.selectedMovie)
-    return (
-      
-        <div>
-			{props.selectedMovie.map((movie) => (
-				<div className='movieBox'>
-					{/* <div className='row image-container d-flex justify-content-left m-4 col-4'>
-						<img src={movie.Poster} alt='movie'></img>
-					</div> */}
-					<div className = 'row'>
-						<h4>{movie.Title}</h4>
-						<p>{movie.Year}</p>
-            <div className='row'>
-            {<RemoveBtn movie={movie} />}
-            </div>
+  return (
 
-						
-					</div>
-					
-				</div>
-			))}	
-      </div>
-    )
+    <div>
+      {props.selectedMovie.map((movie) => (
+        <div className='movieBox'>
+          <div className='image-container d-flex justify-content-left m-4 col-4'>
+            <img src={movie.Poster} alt='movie'></img>
+          </div>
+          <div className='movieInfo'>
+            <h4>{movie.Title}</h4>
+            <p>{movie.Year}</p>
+            <div className='row btncenter'>
+              {<RemoveBtn movie={movie} />}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
   return {
-      selectedMovie: state.selectedMovie
+    selectedMovie: state.selectedMovie
   };
 };
 
 function mapDispatchToProps(dispatch) {
   return {
-      dispatch: dispatch
+    dispatch: dispatch
   };
 }
 
